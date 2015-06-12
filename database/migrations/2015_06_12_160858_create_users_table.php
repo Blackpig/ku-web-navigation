98b671c839f81df82ff59e65c65cc25bd7ab5a8f
Stuart Hallewell <stuart@logicspot.com>
2015-06-12 18:32:45 +0200

    Add user migrations

---
+++ database/migrations/2015_06_12_160858_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+            $table->string('username')->primary();
+            $table->string('name',100);
+            $table->string('email',100)->unique();
+            $table->tinyinteger('employee_type');
+            $table->string('employee_status', 15);
+            $table->string('homedir')->nullable();
+            $table->string('department',100)->nullable();
+            $table->string('avatar');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
+
+}
