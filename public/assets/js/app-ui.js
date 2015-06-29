7314fdcf4d622db750ff40e282abc4947d37976f
Black Pig <stuart@blackpig.eu>
2015-06-29 13:02:43 +0100

    Search updates

--- public/assets/js/app-ui.js
+++ public/assets/js/app-ui.js
@@ -458,6 +458,7 @@ app.directive('packery', ['$rootScope', '$timeout',
 
 		    	var orderItems = function() {
 		    		var itemElems = $rootScope.packery.getItemElements();
+		    		$('#wall').show();
 		    	};
 
 		    	$rootScope.packery.on('layoutComplete', orderItems);
