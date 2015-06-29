904fe22c351ed29471b7d4ec709ff1638e749a15
Black Pig <stuart@blackpig.eu>
2015-06-29 10:58:12 +0100

    Fix icon name

--- app/Http/Models/Endpoint.php
+++ app/Http/Models/Endpoint.php
@@ -110,7 +110,14 @@ class Endpoint extends Model{
 	public static function Search($term, $staff_only)
 	{
 		$db = self::Connection();
-		$rs = \DB::select('EXEC sp_searchEndpoints ?, ?', [$term, $staff_only]);
+
+		$endpoints = [];
+ 		
+ 		foreach (\DB::select('EXEC sp_OrgServiceGroup_Endpoints ?,?', [$guid, $organisation_guid]) as $rs) 
+        {
+            $rs->color = ($rs->color) ? $rs->color : self::assignColour(mt_rand(0,6));
+            $endpoints[] = $rs;
+        }
 
 		return $rs;
 
