4f9a5b40e7750ee667f8af8765c1f8a46bb7f467
Stuart Hallewell <stuart@logicspot.com>
2015-07-03 16:40:05 +0200

    icon selector fixes

--- resources/views/emails/icon-selector.blade.php
+++ resources/views/emails/icon-selector.blade.php
@@ -1,11 +1,11 @@
-Hello,
+<p>Hello,</p>
 
-The following icons have requested to be used use in the Web Navigation portal
+<p>The following icons have requested to be used use in the Web Navigation portal:</p>
 
-Organisation or Channel: {{ $dept }}
-First choice icon: {{ $primary }}
-Alternate choice icon: {{ $primary }}
+<p>Organisation or Channel: {{ $dept }}<br />
+First choice icon: {{ $primary }}<br />
+Alternate choice icon: {{ $primary }}</p>
 
-Icon choices were made by <a href="mailto:{{ $email }}">{{ $requester }}</a> on {{ $date }}
+<p>Icon choices were made by <a href="mailto:{{ $email }}">{{ $requester }}</a> on {{ $date }}</p>
 
-**** This is an automatically generated message - please do not reply ****
+<p><b>**** This is an automatically generated message - please do not reply ****</b></p>
