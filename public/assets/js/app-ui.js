cc5a2ce896acf38349b1f1449ffbf343b2cc8ce7
Stuart Hallewell <stuart@logicspot.com>
2015-07-09 18:14:43 +0200

    JS tweaks

--- public/assets/js/app-ui.js
+++ public/assets/js/app-ui.js
@@ -200,8 +200,9 @@ app.controller('stateCtrl', ['$rootScope','$scope', '$stateParams', '$state', 'u
 
 	self.goToUrl = function($event) {
 		var _t = angular.element($event.currentTarget);
-		var kidz = _t.find('a');
-		console.log(kidz);
+		var _a = _t.find('a');
+		var _url = _a.attr('href');
+		console.log(_url);
 	}
 
 
