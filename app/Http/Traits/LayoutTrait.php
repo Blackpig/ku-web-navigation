b4683c59e431ec7d1b09ab23c55aa8bb76c8c0cd
Stuart Hallewell <stuart@logicspot.com>
2015-07-13 18:29:39 +0200

    Admin layout

--- app/Http/Traits/LayoutTrait.php
+++ app/Http/Traits/LayoutTrait.php
@@ -8,7 +8,6 @@ trait LayoutTrait
 	
 	public static function arrangeToLayout($guid=false, $endpoints)
 	{
-		echo count($endpoints)."--";
 
 		if (!$guid) return collect($endpoints);
 
@@ -23,20 +22,21 @@ trait LayoutTrait
 		$show_always = $endpoints->filter(function ($endpoint) {
     		return $endpoint->always_show == 1;
 		});
-echo count($show_always)."--";
+
 		$arrange = $endpoints->filter(function ($endpoint) {
     		return $endpoint->always_show == 0;
 		});
-echo count($arrange)."--";
+
 		// Turn the collection into a keyed collection by guid
 		
 		foreach($arrange as $item) {
 			$keyed_arrange[$item->guid] = $item;
 		}
 		//Arrange according to the layout
-echo count($keyed_arrange)."--";
+
 		$layout = json_decode($layout->layout, true);
-echo count($layout)."--";
+
+var_dump($keyed_arrange);dd($layout);
 		foreach($layout as $key) {
 
 			$ordered[] = $keyed_arrange["$key"];
