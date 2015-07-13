48ff3cd48ed08f1bcf2ff6cd3cb20ded9ee2ebe9
Stuart Hallewell <stuart@logicspot.com>
2015-07-13 14:54:08 +0200

    Admin layout

--- app/Http/Models/Endpoint.php
+++ app/Http/Models/Endpoint.php
@@ -41,6 +41,10 @@ class Endpoint extends Model{
         foreach (\DB::select('EXEC sp_Organisation_Endpoints ?,?', [$guid,'0']) as $rs) 
         {
             $rs->color = ($rs->color) ? $rs->color : self::assignColour(mt_rand(0,6));
+            $i++;
+            if ($i <= 4) {
+            	$rs->always_show = 1;
+            }
             $endpoints[] = $rs;
         }
 
