fb0fb769e55e42872935d93303bc8c6528edeb48
Stuart Hallewell <stuart@logicspot.com>
2015-07-14 13:50:53 +0200

    Admin layout

--- app/Http/Traits/LayoutTrait.php
+++ app/Http/Traits/LayoutTrait.php
@@ -35,12 +35,11 @@ trait LayoutTrait
 		
 		//Arrange according to the layout
 		$layout = json_decode($layout->layout, true);
-		$keyed_arrange = collect($keyed_arrange);
 
 		foreach($layout as $key) {
 			$ordered[] = $keyed_arrange[$key];
 			//remove the entry from the array
-			$discard = $keyed_arrange->pull($key);
+			unset($keyed_arrange[$key]);
 		}
 
 		// If there are any entries left - new endpoints have been added since 
