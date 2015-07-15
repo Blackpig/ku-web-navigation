6b561d8ca929044c1e67e01663568d62b7374db2
Stuart Hallewell <stuart@logicspot.com>
2015-07-15 13:53:20 +0200

    test user image

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -106,7 +106,7 @@ class UserController extends Controller
 
             $exists = @get_headers($image);
 
-            echo app->basePath();
+            echo app::basePath();
 
             if (strtoupper($exists[0]) == "HTTP/1.1 200 OK") {
                 $hash = md5($id, env('APP_KEY'));
