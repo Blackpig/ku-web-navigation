49400519261a42aa29acb3c6f44a0b1392cc49d6
Stuart Hallewell <stuart@logicspot.com>
2015-08-05 16:35:35 +0200

    Multiple adds

--- app/Http/Models/User.php
+++ app/Http/Models/User.php
@@ -22,7 +22,7 @@ class User extends Model implements AuthenticatableContract {
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['username', 'first_name', 'last_name', 'email', 'employee_type', 'employee_status', 'homedir', 'department', 'avatar', 'gender'];
+	protected $fillable = ['username', 'first_name', 'last_name', 'email', 'external_email', 'employee_type', 'employee_status', 'employee_class', 'job_title', 'department', 'tel', 'avatar', 'gender'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
