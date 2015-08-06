3ae975761751eb47b2d363f61377ac418980bc5c
Stuart Hallewell <stuart@logicspot.com>
2015-08-06 17:07:49 +0200

    Automated tasks

--- app/Console/Commands/EndpointsReview.php
+++ app/Console/Commands/EndpointsReview.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use App\Http\Models\Endpoint;
  
 class EndpointsReview extends Command
