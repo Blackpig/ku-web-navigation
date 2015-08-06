fd11267db47c5bf9f668d2537a89a7168ca3b024
Stuart Hallewell <stuart@logicspot.com>
2015-08-06 18:14:01 +0200

    Automated tasks

--- app/Console/Commands/EndpointsVerify.php
+++ app/Console/Commands/EndpointsVerify.php
@@ -1,5 +1,7 @@
 <?php namespace App\Console\Commands;
  
+use Mail;
+use Illuminate\Contracts\Mail\Mailer;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
@@ -7,40 +9,49 @@ use App\Http\Models\Endpoint;
  
 class EndpointsVerify extends Command
 {
+
+
      /**
      * The console command name.
      *
      * @var string
      */
-     protected $name = 'endpoints:verify';
+     protected $signature = 'endpoints:verify';
  
      /**
      * The console command description.
      *
      * @var string
      */
-     protected $description = "Tests Endpoint URLs are not broken";
+     protected $description = "Send email to endpoint owner if link is broken";
  
      /**
      * Execute the console command.
      *
      * @return void
      */
-     public function handle()
+     public function Handle(Mailer $mailer)
      {
-          
+
+          $client = new \GuzzleHttp\Client();
+
           foreach (Endpoint::AllEndpoints() as $ep) {
                
-               $client = new \GuzzleHttp\Client();
                $res = $client->get($ep->url);
                
                if ($res->getStatusCode() == 404) {
 
-                    $this->dispatch(new SendIsBrokenEmail($ep));
+                    $mailer->send('emails.endpoints.broken_link', ['endpoint' => $endpoint], function ($message) use ($endpoint) {
+                         $message->from('noreply@kingston.ac.uk', $name = null);
+                         $message->sender('noreply@kingston.ac.uk', $name = null);
+                         $message->to($endpoint->primary_email, $name = $endpoint->primary_contact);
+                         $message->subject('KU Navigator Endpoint broken link');
+                      });
+                    
                     Endpoint::SetIsBtoken($ep->guid);
 
                }
           }  
      }
-
+      
 }
--- resources/views/emails/endpoints/is-broken.blade.php
+++ resources/views/emails/endpoints/is-broken.blade.php
@@ -4,20 +4,17 @@
 	<meta charset="utf-8">
 </head>
 <body>
-<p style="color:red">The review date for the following KU Navigator endpoint has passed, and the review is now overdue.</p>
+<p style="color:red">The following endpoint is pointing to a link that is broken.</p>
 
-<p>Review Date: {{ endpoint.review_date }}<br />
-Display name: <a href="">{{ endpoint.title }}</a>
+<p>The specified link may have moved, been removed or you may have mis-typed it on entry.</p>
+
+<p>Display name: <a href="">{{ $endpoint->name }}</a> <small>(Click to edit endpoint)</small><br />
+	Link target: <a href="">{{ $endpoint->url }}</a> <small>(Click to visit the link)</small>
 </p>  
  
 <p>You are receiving this email because you are named as the primary point of contact for the endpoint.</p>
  
-<p>Please click on the above link to take one of the following actions:</p>
-<ul>
-	<li>Confirm that the endpoint is still required, and the URL it directs to is still relevant by extending the review date into the future</li>
-	<li>Or, update, retire or reassign the endpoint.</li>
-</ul>
- 
+<p>Please click on the endpoint edit link above to correct the target link</p>
  
 <p>For more information on KU Navigator, click here <a href=""></a></p>
 </body>
