798006d8b1f900db066846ab0b6b060a56ad5842
Stuart Hallewell <stuart@logicspot.com>
2015-08-07 15:49:17 +0200

    My Tickets

--- app/Http/Models/Ticket.php
+++ app/Http/Models/Ticket.php
@@ -17,8 +17,8 @@ class Ticket extends Model{
 	{
 	
 		$service_desk = $this->getServiceDesk($id, $count);
-		/*$esd = $this->getESD($id, $count);*/
-		$quemis = $this->getQuemis($id, $count);
+		$esd = $this->getESD($id, $count);
+		//$quemis = $this->getQuemis($id, $count);
 
 		$tickets = $service_desk;
 
@@ -36,7 +36,42 @@ class Ticket extends Model{
 
 	private function getESD($id, $count=false)
 	{
-		DB::connection('landesk');
+		$id = "KU39220";
+		$db = env('DB_ESD_DATABASE');
+		$user = env('DB_ESD_USERNAME');
+		$pass = env('DB_ESD_PASSWORD');
+
+    	$conn = oci_connect($user, $pass, $db);
+
+    	if (!$conn) {
+	        $e = oci_error();
+	        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	    } else {
+
+			$sql = 
+				"SELECT * 
+				FROM (
+						SELECT 
+							HCL.HCL_CODE as reference, 
+							HCL.HCL_CRED, 
+							'Ticket details withheld...' as summary, 
+							CCS_NAME
+						FROM HCL 
+							INNER JOIN CCN on (CCN_CODE = HCL_CCNC) 
+							INNER JOIN CCS ON (HCL.HCL_CCSC = CCS_CODE)
+						WHERE CCN_CODE = '$id' 
+						ORDER BY HCL.HCL_CODE DESC)
+				WHERE ROWNUM <= 30)";
+
+				$rs = oci_parse($conn, $sql);
+        		oci_execute($rs);    
+
+        		while ($row = oci_fetch_array($rs, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        			var_dump($row);
+        			echo "<br><br><br>";
+        		};
+        }
+
 		return true;		
 	}
 
@@ -58,7 +93,12 @@ class Ticket extends Model{
 				"SELECT * 
 				FROM (
 					SELECT  
-						h.request_num as reference, to_char(h.date_raised, 'dd/mm/yy hh:mi') as created_at,l.loc_ref as title, h.desc_of_request as summary,  s.status as status, 'Quemis' as source 
+						h.request_num as reference, 
+						to_char(h.date_raised, 'dd/mm/yy hh:mi') as created_at,
+						l.loc_ref as title, 
+						h.desc_of_request as summary,  
+						s.status as status, 
+						'Quemis' as source 
 					FROM ops_hd_help_desk h
 						LEFT OUTER JOIN ops_hd_job j ON j.request_num=h.request_num
 						LEFT OUTER JOIN sched_o_job_status s ON s.status_id=j.status_id
@@ -68,7 +108,7 @@ class Ticket extends Model{
 						c.staff_number = '$id'
 						AND (s.status <> '6 Work order finished ' AND s.status <> '7 Cancelled') 
 					ORDER BY h.date_raised DESC) 
-				WHERE rownum < 31";
+				WHERE ROWNUM <= 30";
 
 				$rs = oci_parse($conn, $sql);
         		oci_execute($rs);    
