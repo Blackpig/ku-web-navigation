c59a7fa012ef8767ed6aeb6e7eb856c274ceeb87
Stuart Hallewell <stuart@logicspot.com>
2015-08-07 14:23:42 +0200

    My Tickets

--- app/Http/Models/Ticket.php
+++ app/Http/Models/Ticket.php
@@ -17,8 +17,8 @@ class Ticket extends Model{
 	{
 	
 		$service_desk = $this->getServiceDesk($id, $count);
-		/*$esd = $this->getESD($id, $count);
-		$quemis = $this->getQuemis($id, $count);*/
+		/*$esd = $this->getESD($id, $count);*/
+		$quemis = $this->getQuemis($id, $count);
 
 		$tickets = $service_desk;
 
@@ -31,7 +31,6 @@ class Ticket extends Model{
 		DB::connection('landesk');
 
 		$rs = DB::select('EXEC sp_getTicketsById ?, ?', [$id, $count]);
-		dd($rs);
 		return $rs;	
 	}
 
@@ -43,7 +42,40 @@ class Ticket extends Model{
 
 	private function getQuemis($id, $count=false)
 	{
-		DB::connection('landesk');
+		$id = "KU39220";
+		$db = env('DB_QMS_DATABASE');
+		$user = env('DB_QMS_USERNAME');
+		$pass = env('DB_QMS_PASSWORD');
+
+    	$conn = oci_connect($user, $pass, $db);
+
+    	if (!$conn) {
+	        $e = oci_error();
+	        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	    } else {
+
+			$sql = 
+				"SELECT * 
+				FROM (
+					SELECT  
+						h.request_num, to_char(h.date_raised, 'dd/mm/yy hh:mi'),l.loc_ref, h.desc_of_request,  s.status  
+					FROM ops_hd_help_desk h
+						LEFT OUTER JOIN ops_hd_job j ON j.request_num=h.request_num
+						LEFT OUTER JOIN sched_o_job_status s ON s.status_id=j.status_id
+						INNER JOIN core_staff c ON c.staff_id=h.orig_staffid
+						INNER JOIN bd_location l ON l.loc_id=h.loc_id
+					WHERE 
+						c.staff_number = '$id'
+						AND (s.status <> '6 Work order finished ' AND s.status <> '7 Cancelled') 
+					ORDER BY h.date_raised DESC) 
+				WHERE rownum < 31";
+
+				$rs = oci_parse($conn, $sql);
+        		oci_execute($rs);    
+
+        		dd($rs)
+        }
+
 		return true;		
 	}
 
