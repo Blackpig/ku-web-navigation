b826e9f7d79be1619501b7b5e79f86b7c862e6e7
Stuart Hallewell <stuart@logicspot.com>
2015-08-11 16:17:29 +0200

    Session timeoiuts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
     public function authenticate($api = true)
     {	
 
+        dd($_SERVER);
 
         if (array_key_exists('PHP_AUTH_USER', $_SERVER)) {
             $username = str_replace("@KUDS.KINGSTON.AC.UK", "", strtoupper($_SERVER['PHP_AUTH_USER'])); 
