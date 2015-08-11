3dc6a99f8e2599cfce32b508df11d5c845626921
Stuart Hallewell <stuart@logicspot.com>
2015-08-11 16:50:30 +0200

    Session timeoiuts

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -255,7 +255,7 @@ class EndpointController extends Controller
 
             if ($is_index) {
                 // [square, square, wide, tall]
-                $mods = ["3.5", "5", "7", "9"];
+                $mods = ["3.5", "5", "6", "8"];
             } elseif ($has_service_group) {
                 $mods = ["5", "5", "7", "9"];                
             } else {
