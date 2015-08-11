ed3375b6ee8cba3406a8ba74da30eaf61a913cbb
Stuart Hallewell <stuart@logicspot.com>
2015-08-11 16:22:06 +0200

    Session timeoiuts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,80 +15,83 @@ class UserController extends Controller
     public function authenticate($api = true)
     {	
 
-        dd($_SERVER);
-
-        if (array_key_exists('PHP_AUTH_USER', $_SERVER)) {
-            $username = str_replace("@KUDS.KINGSTON.AC.UK", "", strtoupper($_SERVER['PHP_AUTH_USER'])); 
+       if (\Auth::check()) {
+            $user = \Auth::user()->id;
         } else {
-            $msg = "User not authenticated or session expired. Please reload the page";
-             \Log::error('API Error 401 - Authentication',["context"=>$msg]);
-            return $this->respondError(401, $msg);
-            exit;
-        }
 
-        
-        if (!$username) {
-            $msg = "No username available for LDAP lookup";
-            \Log::error('API Error 401 - Authentication',["context"=>$msg]);
-            return $this->respondError(401,$msg);
-            exit;
-        }
-
-        //$username = ($username=='KA01356') ? "K1068743" : $username;
-
-        /* Do we have the user within the app*/
-        $user = User::find($username);
-
-        /* if no then retrieve from LDAP and create*/
-        if (!$user) {
-            include base_path() . "/vendor/adldap/adldap/lib/adLDAP/adLDAP.php";
-
-            $ldap_options = [
-                "domain_controllers" => [env('LDAP_DC_1'),env('LDAP_DC_2')],
-                "base_dn" => env('LDAP_BASE_DN'),
-                "account_suffix" => env('LDAP_USER_SUFFIX'), 
-                "admin_username" => env('LDAP_ADMIN_USER'),
-                "admin_password" => env('LDAP_ADMIN_PASS'),
-                "use_ssl" => true
-            ];
-
-            $ldap = new \adLDAP\adLDAP($ldap_options);
-
-            $ldap_user = $ldap->user()->infoCollection($username, array("*"));
-
-
-            if(!$ldap_user) {
-                $msg = "Unable to find user id: $username in LDAP lookup";
-                \Log::error('API Error 401 - Authentication',["context"=>$msg]);
+            if (array_key_exists('PHP_AUTH_USER', $_SERVER)) {
+                $username = str_replace("@KUDS.KINGSTON.AC.UK", "", strtoupper($_SERVER['PHP_AUTH_USER'])); 
+            } else {
+                $msg = "User not authenticated or session expired. Please reload the page";
+                 \Log::error('API Error 401 - Authentication',["context"=>$msg]);
                 return $this->respondError(401, $msg);
                 exit;
             }
 
-            $user = new User;
-            $user->id = $username;
-            $user->first_name = $ldap_user->givenname;
-            $user->last_name = $ldap_user->sn;
-            $user->email = $ldap_user->mail;
-            $user->external_email = $ldap_user->msexchextensionattribute21;
-            $user->employee_type = $this->getEmployeeType($ldap_user->employeetype);
-            $user->employee_class = ucwords(strtolower($ldap_user->employeetype));
-            $user->employee_status = ($user->employee_type == 0) ? 'staff' : 'student';
-            $user->job_title = ucwords(strtolower($ldap_user->title));
-            $user->department = ucwords(strtolower($ldap_user->company));
-            $user->tel = $ldap_user->telephonenumber;
-            $user->gender = strtolower($ldap_user->extensionAttribute1);
-            $user->avatar = $this->getUserImage($user->id, $user->gender);
-            $user->save();
             
+            if (!$username) {
+                $msg = "No username available for LDAP lookup";
+                \Log::error('API Error 401 - Authentication',["context"=>$msg]);
+                return $this->respondError(401,$msg);
+                exit;
+            }
+
+            //$username = ($username=='KA01356') ? "K1068743" : $username;
+
+            /* Do we have the user within the app*/
             $user = User::find($username);
-        }
 
-        if (strpos($user->avatar, 'avatar_') !== 0 ) {
-            $user->avatar = $this->getUserImage($user->id, strtolower($user->gender));
-            $user->save();
-        }
+            /* if no then retrieve from LDAP and create*/
+            if (!$user) {
+                include base_path() . "/vendor/adldap/adldap/lib/adLDAP/adLDAP.php";
 
-        \Auth::login($user);
+                $ldap_options = [
+                    "domain_controllers" => [env('LDAP_DC_1'),env('LDAP_DC_2')],
+                    "base_dn" => env('LDAP_BASE_DN'),
+                    "account_suffix" => env('LDAP_USER_SUFFIX'), 
+                    "admin_username" => env('LDAP_ADMIN_USER'),
+                    "admin_password" => env('LDAP_ADMIN_PASS'),
+                    "use_ssl" => true
+                ];
+
+                $ldap = new \adLDAP\adLDAP($ldap_options);
+
+                $ldap_user = $ldap->user()->infoCollection($username, array("*"));
+
+
+                if(!$ldap_user) {
+                    $msg = "Unable to find user id: $username in LDAP lookup";
+                    \Log::error('API Error 401 - Authentication',["context"=>$msg]);
+                    return $this->respondError(401, $msg);
+                    exit;
+                }
+
+                $user = new User;
+                $user->id = $username;
+                $user->first_name = $ldap_user->givenname;
+                $user->last_name = $ldap_user->sn;
+                $user->email = $ldap_user->mail;
+                $user->external_email = $ldap_user->msexchextensionattribute21;
+                $user->employee_type = $this->getEmployeeType($ldap_user->employeetype);
+                $user->employee_class = ucwords(strtolower($ldap_user->employeetype));
+                $user->employee_status = ($user->employee_type == 0) ? 'staff' : 'student';
+                $user->job_title = ucwords(strtolower($ldap_user->title));
+                $user->department = ucwords(strtolower($ldap_user->company));
+                $user->tel = $ldap_user->telephonenumber;
+                $user->gender = strtolower($ldap_user->extensionAttribute1);
+                $user->avatar = $this->getUserImage($user->id, $user->gender);
+                $user->save();
+                
+                $user = User::find($username);
+            }
+
+            if (strpos($user->avatar, 'avatar_') !== 0 ) {
+                $user->avatar = $this->getUserImage($user->id, strtolower($user->gender));
+                $user->save();
+            }
+
+            \Auth::login($user);
+        }
 
         if ($api) {
             return $this->respondOK($user);
