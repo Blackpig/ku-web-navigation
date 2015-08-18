ebef883b74c640bd92fe5684d110f8de2095081b
Stuart Hallewell <stuart@logicspot.com>
2015-08-18 19:56:56 +0200

    ie css

--- public/assets/js/app-ui.js
+++ public/assets/js/app-ui.js
@@ -580,7 +580,7 @@ $(document).ready(function(){
 
 	/* If the browser doesn't support 3d transforms then the front/back tile animation will not fire */
 	/* We will manually toggle the front and back of teh tiles */
-	$('.fa-info').click(function(e){
+	$('a.info-trigger').click(function(e){
 			e.preventDefault();
 			console.log('cliekty');
 			if ( $('html').hasClass('no-csstransforms3d')) {
