114ffecc09749409a3b7db8e7fd4377d11a26402
Stuart Hallewell <stuart@logicspot.com>
2015-08-21 17:43:22 +0200

    my status small devices

--- public/assets/js/app-ui.js
+++ public/assets/js/app-ui.js
@@ -141,11 +141,11 @@ var app = angular.module('kuwnApp', ['ui.router', 'mm.foundation'])
 					})
 		.run(
 			['$rootScope',
-				function($rootScopr(){
+				function($rootScope) {
 					$rootScope.$on('$stateChangeSuccess', function() {
 					   document.body.scrollTop = document.documentElement.scrollTop = 0;
 					})
-				})
+				}
 			]
 		)	
 	}]);
