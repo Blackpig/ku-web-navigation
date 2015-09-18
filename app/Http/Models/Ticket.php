cbcf3d805338c93b0217efac752495f3e33f8339
Stuart Hallewell <stuart@logicspot.com>
2015-09-18 12:25:06 +0200

    quemis

--- app/Http/Models/Ticket.php
+++ app/Http/Models/Ticket.php
@@ -141,7 +141,7 @@ class Ticket extends Model{
        			$r = oci_execute($rs);    
 
        			if (!$r) {
-       				$e = oci_error($rs)
+       				$e = oci_error($rs);
        			} else {
 	        		while ($row = oci_fetch_array($rs, OCI_ASSOC+OCI_RETURN_NULLS)) {
 	        			$item = new \stdClass();
