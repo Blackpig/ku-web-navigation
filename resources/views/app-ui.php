33a76abd0286fd30624dd1337d7c6b514b44cad4
Stuart Hallewell <stuart@logicspot.com>
2015-09-22 19:09:04 +0200

    add angulartics

--- resources/views/app-ui.php
+++ resources/views/app-ui.php
@@ -36,7 +36,7 @@
     <script src="/assets/vendor/angular/angular.js"></script>
     <script src="/assets/vendor/angular-ui/angular-ui-router.min.js"></script>
     <script src="/assets/vendor/angulartics/src/angulartics.js"></script>
-    <script src="/assets/vendor/angulartics/src/angulartics=piwik.js"></script>
+    <script src="/assets/vendor/angulartics/src/angulartics-piwik.js"></script>
     <script src="/assets/vendor/pineconellc/mm-foundation-tpls-0.6.0.js"></script>
     <script src="/assets/vendor/metafizzy/packery.pkgd.min.js"></script>
     <script src="/assets/js/app-ui.js"></script>
