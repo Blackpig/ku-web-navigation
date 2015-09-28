869b33f4cfdc8c095dc50b371de53a8935de5e6b
Stuart Hallewell <stuart@logicspot.com>
2015-09-28 22:53:51 +0200

    Piwk amend

--- resources/views/app-ui.php
+++ resources/views/app-ui.php
@@ -22,7 +22,7 @@
           //_paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-            var u="//" + window.location.hostname + "/analytics/";
+            var u="//kunavigator.kingston.ac.uk/analytics/";
             _paq.push(['setTrackerUrl', u+'piwik.php']);
             _paq.push(['setSiteId', 1]);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
